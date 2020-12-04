02036a8407f4fc7c1856672672530bbf28d3f766
Dariusz Kupidura <dariusz.kupidura@gmail.com>
2020-12-04 10:16:10 +0100

    Zmiana nazwy

--- src/main/java/pl/decerto/recruit/dk/MergerApplication.java
+++ src/main/java/pl/decerto/recruit/dk/MergerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DkApplication {
+public class MergerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DkApplication.class, args);
+        SpringApplication.run(MergerApplication.class, args);
     }
 
 }
